e5a9975592c548dad20608dc39be10a39b7c95f7
Rubich Viktor <syzspectroom@gmail.com>
2015-01-30 21:09:00 +0200

    payments and basic model

--- application.rb
+++ application.rb
@@ -20,12 +20,37 @@ end
 # root page
 get '/' do
   @photos = Photo.all
+  @profile = Profile.first
   haml :root
 end
 
+post '/submit' do
+  profile = Profile.new
+  profile.name = params[:name]
+  profile.surname = params[:surname]
+  profile.adress = params[:adress]
+  profile.phone = params[:phone]
+  profile.email = params[:email]
+  profile.site = params[:site]
+  profile.photo_alliance = params[:photo_alliance]
+  profile.position = params[:position]
+
+  if profile.save
+
+    redirect profile.payment_url
+  end
+
+end
+
+post "/payment/:id" do
+  profile = Profile.get(params[:id])
+  if profile.signature_valid?( params[:signature], params[:data] )
+    profile.paid = true
+    profile.save
+  end
+end
 
 post '/upload' do
-
   upload = Photo.new
   upload.file = params[:image]
   upload.save
--- lib/profile.rb
+++ lib/profile.rb
@@ -2,10 +2,65 @@
 class Profile
   include DataMapper::Resource
 
-  property :id,         Serial
-  property :name,       String  
-  property :created_at, DateTime
-  property :updated_at, DateTime
+  property :id,             Serial
+  property :name,           String
+  property :surname,        String
+  property :adress,         String
+  property :phone,          String
+  property :email,          String
 
-  validates_presence_of :name
+  property :site,           String
+  property :photo_alliance, String
+  property :position,       String
+  property :paid,           Boolean, default: false
+
+  property :created_at,     DateTime
+  property :updated_at,     DateTime
+
+  validates_presence_of :name, :surname, :adress, :phone, :email
+
+  def payment_url
+    base = request_params
+
+    "https://www.liqpay.com/api/checkout" +
+      "?data=#{ URI::encode( base ) }" +
+      "&signature=#{ URI::encode( signature( base ) ) }"
+  end
+
+  protected
+
+    def rate
+      650
+    end
+
+    def request_params
+      params = {
+        version: 3,
+        public_key: 'public_key',
+        amount: rate,
+        currency: 'UAH',
+        description: self.id,
+        language: 'ru',
+        order_id: order_id,
+        server_url: "http://localhost:4567/payment/#{self.id}",
+        result_url: "http://localhost:4567",
+        sandbox: 1
+      }
+      json = JSON.generate( params )
+      Base64.encode64( json ).gsub("\n",'')
+    end
+
+    def order_id
+      Time.now.to_s.parameterize + self.id.to_s
+    end
+
+    def signature_valid?(recieved_signature, recieved_data)
+      signature(recieved_data) == recieved_signature
+    end
+
+    def signature(data)
+      key = 'private_key' + data + 'private_key'
+      key = Digest::SHA1.digest( key )
+      Base64.encode64( key ).gsub("\n",'')
+    end
 end
